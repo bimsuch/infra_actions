dc1b062572ace0ffbd9fc6bf14d08a3896d39cda
Андрей Бычков <bimsuch@gmail.com>
2023-06-05 22:25:47 +0300

    test test2

--- infra_project/infra_project/urls.py
+++ infra_project/infra_project/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-git pushurlpatterns = [
+urlpatterns = [
     path('', include('infra_app.urls', namespace='infra_app')),
     path('admin/', admin.site.urls),
 ]
